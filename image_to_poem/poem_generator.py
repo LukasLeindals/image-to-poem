291209660e132b5b86ec1b69b8581c4ec1862431
AnaTeodoraR <97507962+AnaTeodoraR@users.noreply.github.com>
2023-11-20 16:48:00 +0100

    added the BERT similarity scoring

--- image_to_poem/poem_generator.py
+++ image_to_poem/poem_generator.py
@@ -1,9 +1,13 @@
 from transformers import pipeline
+from sentence_transformers import SentenceTransformer, st_util
+import numpy as np
 
 class PoemGenerator:
-    def __init__(self):
+    def __init__(self, n_candidates: int=10):
         print("Initializing image-to-text model...")
         self.image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+        
+        self.N = n_candidates
         
     def image_to_poem(self, image_paths):
         # make sure image_paths is a list
@@ -26,13 +30,32 @@ class PoemGenerator:
     
     def generate_poem(self, description: str):
         # print(description)
+        
+        # poems = self.get_candidates()
+        # poem = self.select_poem(poems)
+        
+        # return poem 
         return description
     
     def get_candidates():
+        # call GPT 2 ? => make N candidates 
         pass
     
-    def select_poem():
-        pass
+    def select_poem(candidates: list, description: str):
+        # initialize BERT model
+        bert = SentenceTransformer('all-MiniLM-L6-v2')
+        
+        # get poem embeddings and description embedding 
+        poem_emb = bert.encode(candidates)
+        desc_emb = bert.encode(description)
+        
+        # calculate similarity
+        similarities = st_util.cos_sim(poem_emb, desc_emb)
+        similarities = np.array(similarities).flatten()
+        
+        # get the "best" = most similar candidate 
+        best = np.argmax(similarities)
+        return candidates[best]
         
 if __name__ == "__main__":
     # initialize poem generator
