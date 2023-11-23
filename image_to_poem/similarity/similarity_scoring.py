95a53528823dbd750e01690c1f7a202ccd859136
AnaTeodoraR <97507962+AnaTeodoraR@users.noreply.github.com>
2023-11-23 18:45:09 +0100

    Update similarity_scoring.py

--- image_to_poem/similarity/similarity_scoring.py
+++ image_to_poem/similarity/similarity_scoring.py
@@ -4,29 +4,31 @@ from bert_classifier import Bert_classifier
 
 
 class BertSimilarityModel:
-    def __init__(self, no_hidden_layers: int=2, hidden_dim: int = 100):
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    def __init__(self, no_hidden_layers: int=2, hidden_dim: int = 100, max_length: int = 150):
+        
+        # models 
         self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.model = Bert_classifier(self.bert, no_hidden_layers, hidden_dim)
         
+        # tokenizer 
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        
+        # other parameters 
+        self.max_len = max_length 
         self.trained = False 
     
-    def tokenize_sequences(self, seq1 : str, seq2 : str) -> list:
-        # tokenize sequences 
-        # add BERT special tokens 
-        # do padding 
-        pass
-    
-    def similarity(self, seq1: str, seq2: str) -> float:
+    def similarity(self, caption: str, poem: str) -> float:
         if not self.trained:
-            print("The bert classifier has not been trained yet")
-        
-        input = self.tokenize_sequences(seq1, seq2)
-        out = self.model.forward(input)
+            print("The BERT classifier has not been trained yet. Similarity might not be good.")
+            
+        encoding = self.tokenizer.encode_plus(caption, poem, max_length=self.max_len, truncation=True, padding="max_length", return_tensors="pt")
+        out = self.model.forward(encoding)
 
         return out
     
     def train_bert_classifier(self):
         self.trained = True 
         # train self.model
-    
+
+
+
